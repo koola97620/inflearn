f1c58c958005964510a73f84ca4f56c868a51851
Jaeyong <koola97620@nate.com>
2019-04-17 23:13:41 +0900

    inflearn-boot 강의

---
+++ inflearn-boot/springbootgettingstarted/src/main/java/me/jdragon/Application.java
@@ -0,0 +1,19 @@
+package me.jdragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author choijaeyong on 17/04/2019.
+ * @project springbootgettingstarted
+ * @description
+ */
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class,args);
+  }
+
+}
