ccbf43924a0dde4ece3f89ff169427688d107c4a
Jaeyong <koola97620@nate.com>
2019-04-22 21:49:20 +0900

    inflearn-boot 강의. 자동설정이해
    spring.factories
    EnableAutoConfiguration

--- inflearn-boot/springbootgettingstarted/src/main/java/me/jdragon/Application.java
+++ inflearn-boot/springbootgettingstarted/src/main/java/me/jdragon/Application.java
@@ -1,6 +1,7 @@
 package me.jdragon;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -13,6 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
+//    SpringApplication application = new SpringApplication(Application.class);
+//    application.setWebApplicationType(WebApplicationType.NONE);
+//    application.run(args);
     SpringApplication.run(Application.class,args);
   }
 
