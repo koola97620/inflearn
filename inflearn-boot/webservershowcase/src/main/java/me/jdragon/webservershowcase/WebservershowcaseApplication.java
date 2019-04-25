91a92c463effccda3622eb8a7a4dd703503cff6c
Jaeyong <koola97620@nate.com>
2019-04-25 13:45:11 +0900

    14. 내장 웹 서버 응용2부 : HTTPS , HTTP2

--- inflearn-boot/webservershowcase/src/main/java/me/jdragon/webservershowcase/WebservershowcaseApplication.java
+++ inflearn-boot/webservershowcase/src/main/java/me/jdragon/webservershowcase/WebservershowcaseApplication.java
@@ -2,12 +2,34 @@ package me.jdragon.webservershowcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class WebservershowcaseApplication {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello spring";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(WebservershowcaseApplication.class, args);
   }
+
+//  @Bean
+//  public ServletWebServerFactory serverFactory() {
+//    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//    tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//    return tomcat;
+//  }
+//
+//  private Connector createStandardConnector() {
+//    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//    connector.setPort(8080);
+//    return connector;
+//  }
+
 
 }
