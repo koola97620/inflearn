70065386e19cc34b6557446ef9e611fdc048b65e
Jaeyong <koola97620@nate.com>
2019-04-28 12:55:23 +0900

    14. 내장 웹 서버 응용2부 : HTTPS , HTTP2

---
+++ inflearn-boot/webservershowcase/src/main/java/me/jdragon/webservershowcase/WebservershowcaseApplication.java
@@ -0,0 +1,13 @@
+package me.jdragon.webservershowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservershowcaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebservershowcaseApplication.class, args);
+  }
+
+}
